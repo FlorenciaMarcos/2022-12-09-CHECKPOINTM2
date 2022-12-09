5e098e7c39821255dc6ca0398bbbeb4ade551a09
Florencia Marcos <florcita6221@gmail.com>
2022-12-09 15:30:44 -0300

    ultimo

--- 2022-12-09-FlorMarcos-master/src/components/DetailCard/DetailCard.jsx
+++ 2022-12-09-FlorMarcos-master/src/components/DetailCard/DetailCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom"
 // CUIDADO! SI O SI FUNCTIONAL COMPONENT! SINO SE ROMPEN LOS TEST EN CASO CONTRARIO!
 
 const DetailCard = (props) => {
   return (
     <div>
+      <img src={props.image}  alt="team-img" />
+      <p>ID: {props.id}</p>
+      <p>Base: {props.base}</p>
+      <p>Drivers: {props.drivers}</p>
+      <p>WorldChampionships: {props.worldChampionships}</p>
+     <Link to="/" > 
+     <button>Volver</button>
+     </Link>
     </div>
   );
 };
